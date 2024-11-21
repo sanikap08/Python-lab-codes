b1b4adcf4a124074457a3afb6f840cdac2d82b3b
Sanika Patil <sanikaapatil10@gmail.com>
2024-11-22 02:27:25 +0530

    changes

---
+++ Lab 4/Q6.py
@@ -0,0 +1,15 @@
+''' Write a program to check if a string contains only digits '''
+
+user_input = input("Enter a string: ")
+if user_input.isdigit():
+    print("The string contains only digits.")
+else:
+    print("The string does not contain only digits.")
+
+
+
+
+
+# Name : Sanika Patil
+# Section K
+# 23FE10CSE00445
