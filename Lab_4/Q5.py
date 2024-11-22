8e360c1ef7d7260c11f47640286d5e5b4d979415
Sanika Patil <sanikaapatil10@gmail.com>
2024-11-22 09:26:41 +0530

    inserted

--- Lab_4/Q5.py
+++ Lab_4/Q5.py
@@ -1,3 +1,13 @@
+'''Write a program to take an input from a user as a string then, reverse it. '''
+
+
 user_input = input("Enter a string: ")
 reversed_string = user_input[::-1]
 print("Reversed string:", reversed_string)
+
+
+
+
+# Name : Sanika Patil
+# Section K
+# 23FE10CSE00445
